6bc90fefe6e42ccdffa911430f078a03df856e34
leo <leo99brizuela@gmail.com>
2023-10-04 23:53:04 -0300

    juego

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 -- main.lua
 local playerLeftColor = {1, 0, 0}  -- Rojo
 local playerRightColor = {0, 0, 1} -- Azul
-local paddleLeft = { x = 50, y = 250, width = 10, height = 60, speed = 400,color=playerLeftColor }
-local paddleRight = { x = 740, y = 250, width = 10, height = 60, speed = 400,color=playerRightColor }
+local paddleLeft = { x = 50, y = 250, width = 10, height = 60, speed = 400,color=playerLeftColor,score=0 }
+local paddleRight = { x = 740, y = 250, width = 10, height = 60, speed = 400,color=playerRightColor, score=0 }
 local gameState = "menu" -- Estado inicial del juego
 local isPaused = false -- Variable para controlar la pausa
 local ball = { x = 400, y = 300, width = 20, height = 20, speedX = 0, speedY = 0 }
@@ -10,6 +10,7 @@ local ballSpeed=300
 local ballSpeedIncrease=50
 local scoreLeft = 0
 local scoreRight = 0
+local winningScore=5
 
 
 function love.load()
@@ -34,11 +35,11 @@ function love.update(dt)
 
         if ball.x < 0 then
             -- Punto para el jugador de la derecha
-            scoreRight = scoreRight + 1
+            paddleRight.score = paddleRight.score+1
             ResetBall()
         elseif ball.x + ball.width > 800 then
             -- Punto para el jugador de la izquierda
-            scoreLeft = scoreLeft + 1
+            paddleLeft.score = paddleLeft.score + 1
             ResetBall()
         end
         
@@ -92,11 +93,21 @@ function love.draw()
         love.graphics.setColor(paddleRight.color)
         love.graphics.rectangle("fill", paddleRight.x, paddleRight.y, paddleRight.width, paddleRight.height)
         love.graphics.setColor(paddleLeft.color)
-        love.graphics.print("Player 1: " .. scoreLeft, 50, 50)
+        love.graphics.print("Player 1: " .. paddleLeft.score, 50, 50)
         love.graphics.setColor(paddleRight.color)
-        love.graphics.print("Player 2: " .. scoreRight, 650, 50)
+        love.graphics.print("Player 2: " .. paddleRight.score, 650, 50)
         love.graphics.setColor(1, 1, 1)
+    elseif gameState == "gameover" then
+    -- Determinar al ganador y mostrar un mensaje
+    local winner
+    if paddleLeft.score == winningScore then
+        winner = "Player 1"
+    else
+        winner = "Player 2"
     end
+
+    love.graphics.printf(winner .. " wins!\nPress Enter to restart", 0, 250, 800, "center")
+end
 
     if isPaused then
         -- Dibuja el texto de pausa
